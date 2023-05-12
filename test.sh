da132770c2e4a58cbe780fdbd65c7016e01004e0
Hieu Pham <minhhieu060799@gmail.com>
2023-05-12 14:51:40 +0700

    sce2

--- test.sh
+++
@@ -1,8 +0,0 @@
-PR_TITLE="[CC] Testing +semver:"
-PR_BODY="Testing 4"
-SEMVER_REGEX="\+semver:(major|minor|patch|pre|build)"
-if ! ([[ "$PR_TITLE" =~ $SEMVER_REGEX ]] || [[ "$PR_BODY" =~ $SEMVER_REGEX ]]); then
-  echo "This PR does not contain a semantic version string."
-  exit 1
-fi
- echo "aaa"
